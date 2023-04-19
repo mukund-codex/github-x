bea8c273d879c69cbd89ebb7601a11ed42293507
dev-inder <inderpreet.s.narula@gmail.com>
2023-04-19 10:52:02 +0530

    Removed login after registration

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -18,7 +18,6 @@ class RegistrationTest extends TestCase
             'password_confirmation' => 'password',
         ]);
 
-        $this->assertAuthenticated();
         $response->assertNoContent();
     }
 }
