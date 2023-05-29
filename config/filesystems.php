245d6f4ea88745603d067f2a4265899c354ca25d
Vikramjeet Singh <vikramjeet@founderandlightning.com>
2023-05-29 11:38:14 +0530

    Modify the key name to avoid conflicts with the S3 build pack on Heroku.

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,11 +46,11 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'key' => env('AWS_ACCESS_KEY'),
+            'secret' => env('AWS_SECRET_KEY'),
+            'region' => env('AWS_REGION'),
+            'bucket' => env('AWS_BUCKET_S3'),
+            'url' => env('AWS_DEFAULT_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
