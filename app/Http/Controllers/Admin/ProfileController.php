2ccbeeeab476ddeab40c28d44fb71fce126feb8e
Mateusz Kuźnik <mateusz.k@founderandlightning.com>
2023-07-05 15:34:03 +0200

    removed dump

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -23,7 +23,6 @@ class ProfileController extends Controller
     public function update(UpdateProfileRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
-        dd($request->validated());
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
             $request->user()->sendEmailVerificationNotification();
