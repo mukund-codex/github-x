1315aae0ee1b662fc8f1a9af5a5fba7c1e3c223f
Mateusz Kuźnik <mateusz.k@founderandlightning.com>
2023-08-17 13:16:17 +0200

    pint fix

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -10,7 +10,6 @@ use App\Traits\HttpResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Str;
 
 class AuthenticatedSessionController extends Controller
 {
